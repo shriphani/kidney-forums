c304ed5880d67ca74dd2f0db7dca044410ea1861
Shriphani Palakodety <shriphanip@gmail.com>
2013-09-21 17:12:06 -0400

    read post pages

--- src/kidney_forums/index_pages.clj
+++ src/kidney_forums/index_pages.clj
@@ -3,7 +3,23 @@
 (ns kidney-forums.index-pages
   (:gen-class :main true)
   (:require [warc-clojure.core :as warc]
-            [clojure.tools.cli :as cli]))
+            [clojure.tools.cli :as cli]
+            [net.cgrand.enlive-html :as html]))
+
+(defn get-post-pages
+  [warc-file]
+  (doseq [record (warc/skip-get-response-records-seq
+                  (warc/get-warc-reader warc-file))]
+    (filter
+     identity
+     (map
+      #(-> %
+           :attrs
+           :href)
+      (-> record
+          :payload-stream
+          html/html-resource
+          (html/select [:a]))))))
 
 (defn -main
   [& args]
@@ -14,4 +30,4 @@
                        #(and (re-find #".warc.gz" (.getAbsolutePath %))
                              (not (re-find #"latest" (.getAbsolutePath %))))
                        (file-seq (java.io.File. warcs-dir)))]
-      (println warc-file))))
+      (println (get-post-pages warc-file)))))
